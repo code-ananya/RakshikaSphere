4738244ed55c11c664be888ee53298cb4575ec89
Ananya Pandey <ananyapandey22222@gmail.com>
2026-03-25 11:56:45 +0530

    Update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://rakshikasphere.vercel.app"
+  ],
+  credentials: true
+}));
+
 
 const start = async () => {
   try {
