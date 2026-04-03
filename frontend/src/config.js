f9dbc27a94f89cafeb0fcbf84fd3d280e3fe2530
Ayushi-Vats <ayushivats509@gmail.com>
2026-04-03 14:37:50 +0530

    Rename REACT_APP_ML_URL to REACT_APP_ML_SERVICE_URL for consistency

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
-const ML_URL  = process.env.REACT_APP_ML_URL  || "http://localhost:5001";
+const ML_URL  = process.env.REACT_APP_ML_SERVICE_URL || "http://localhost:5001";
 
 export { API_URL, ML_URL };
